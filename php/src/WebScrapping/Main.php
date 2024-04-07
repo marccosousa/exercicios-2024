411c9ce24ba9a73a016d1ff4a41d760f3769f2f1
Marco Sousa <marcosousacontato@gmail.com>
2024-04-07 18:32:26 -0300

    Initial commit
    Implementacao inicial do WebScrapping. Ja com a classe Scrapper implementada para extracao de informacoes do HTML.

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,22 +2,26 @@
 
 namespace Chuva\Php\WebScrapping;
 
+libxml_use_internal_errors(true);
+
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main {
+class Main
+{
 
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
+  public static function run(): void
+  {
+    $content = file_get_contents(__DIR__ . '/../../assets/origin.html');
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    $dom->loadHTML($content);
 
     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
     print_r($data);
   }
-
 }
